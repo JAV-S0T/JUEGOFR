6190a071866c97c82acb20a2b709d8247f29aeaf
JAV-S0T <javiersotodiaz21@gmail.com>
2025-01-14 14:31:43 +0100

    Add files via upload

---
+++ Juego FR/Menu/Juego/Juego.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+var change_volver3 = load("res://Menu/Menu_Principal.tscn")
+
+
+
+func _on_volver_3_pressed():
+	get_tree().change_scene_to_packed(change_volver3)
+	pass # Replace with function body.
