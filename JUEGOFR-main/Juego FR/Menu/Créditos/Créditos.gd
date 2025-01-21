0729d3552b794cf2eecbfd9be3b453a1c5ddf180
JAV-S0T <javiersotodiaz21@gmail.com>
2025-01-21 14:31:53 +0100

    Delete JUEGOFR-main/Juego FR directory

--- JUEGOFR-main/Juego FR/Menu/Créditos/Créditos.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-var change_volver2 = load("res://Menu/Menu_Principal.tscn")
-
-func _on_volver_2_pressed():
-	get_tree().change_scene_to_packed(change_volver2)
-	pass # Replace with function body.
