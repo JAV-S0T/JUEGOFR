7a14f7aa00f24469327b7f0069bcd4cdde27baed
JAV-S0T <javiersotodiaz21@gmail.com>
2025-02-05 13:32:48 +0100

    Add files via upload

--- Juego FR/Menu/Juego/Juego.gd
+++ Juego FR/Menu/Juego/Juego.gd
@@ -6,7 +6,24 @@ var change_volver3 = load("res://Menu/Menu_Principal.tscn")
 
 func _on_volver_3_pressed():
 	get_tree().change_scene_to_packed(change_volver3)
-	$"/root/Global/AudioStreamPlayer2D".play()
+	$"/root/Global/SonidoJuego".play()
 	$"/root/Global/BotonClick".play()
+	pass # Replace with function body.
+
+
+
+
+func _on_atacar_pressed():
+	$"/root/Global/BotonClick".play()
+	pass # Replace with function body.
+
+
+func _on_volver_3_mouse_entered():
+	$"/root/Global/BotonEncima".play()
+	pass # Replace with function body.
+
+
+
+func _on_atacar_mouse_entered():
 	$"/root/Global/BotonEncima".play()
 	pass # Replace with function body.
