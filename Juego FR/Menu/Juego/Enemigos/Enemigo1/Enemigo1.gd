6369dd1916a4957cf098cc2ff93670aa2c2d242c
JAV-S0T <javiersotodiaz21@gmail.com>
2025-03-11 14:31:34 +0100

    Add files via upload

--- Juego FR/Menu/Juego/Enemigos/Enemigo1/Enemigo1.gd
+++ Juego FR/Menu/Juego/Enemigos/Enemigo1/Enemigo1.gd
@@ -1,23 +1,26 @@
 extends CharacterBody2D
+
+var attacking = false  # Controlar si el enemigo está atacando o no
+
 func _ready():
 	$Enemigo1.play("idle")
 	pass
-	
-	
-
 
 func _on_enemigo_1_animation_looped():
-	$Enemigo1.play("idle")
-	$Timer.start()
-	pass # Replace with function body.
-
+	if not attacking:
+		$Enemigo1.play("idle")  # Regresa al estado idle si no está atacando
+		$Timer.start()  # Reinicia el temporizador para el próximo turno
+		$Timer2.start()  # Reinicia el temporizador para el siguiente ataque
+	pass
 
 func _on_timer_timeout():
-	$Enemigo1.play("attack")
-	$Timer2.start()
-	pass # Replace with function body.
-
+	if not attacking:  # Asegurarse de que solo ataque si no está en ataque
+		attacking = true
+		$Enemigo1.play("attack")
+		pass
 
 func _on_timer_2_timeout():
-	get_parent().get_node("Player/Player").play("hit")
-	pass # Replace with function body.
+	if attacking:  # Solo aplica daño si está en estado de ataque
+		get_parent().get_node("Player/Player").play("hit")
+		attacking = false  # Resetea el estado después del ataque
+	pass
--- Juego FR/Menu/Juego/Player/Player.gd
+++ Juego FR/Menu/Juego/Player/Player.gd
@@ -1,34 +1,33 @@
 extends CharacterBody2D
 
+var attacking = false # Variable para controlar el estado de ataque
+
 func _ready():
 	$Player.play("idle")
-func _process(delta):
-	
-	
-	pass
 
+func _process(delta):
+	# Aquí podrías agregar la lógica de turno o alguna otra condición
+	pass
 
 func _on_atacar_mouse_entered():
 	$"/root/Global/BotonEncima".play()
-	pass # Replace with function body.
-
-
+	pass
 
 func _on_atacar_pressed():
-	$"/root/Global/BotonClick".play()
-	$Player.play("attack")
-	$Timer.start()
-	
-	pass # Replace with function body.
-
+	if not attacking:  # Solo puede atacar si no está en ataque
+		attacking = true
+		$"/root/Global/BotonClick".play()
+		$Player.play("attack")
+		$Timer.start()  # Inicia el temporizador para controlar el daño
+	pass
 
 func _on_player_animation_looped():
 	$Player.play("idle")
-	pass # Replace with function body.
-
-
+	attacking = false  # Resetea el estado después de terminar el ataque
+	pass
 
 func _on_timer_timeout():
 	$AudioStreamPlayer2D.play()
+	# Aquí se aplica el daño al enemigo
 	get_parent().get_node("Enemigo1/Enemigo1").play("hit")
-	pass # Replace with function body.
+	pass
